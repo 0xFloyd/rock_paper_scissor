59bbb9f13c5fe8ab75ceeb42cac256b5483a9ac5
Ryan Floyd <arfloyd7@gmail.com>
2019-07-26 22:15:53 -0400

    Updated to fix button disabling bug that prevented button from being disabled

--- main.js
+++ main.js
@@ -1,62 +1,127 @@
 
 const buttons = document.querySelectorAll('.buttons');
 let paragraph = document.querySelectorAll('#result');
-document.getElementById("result").innerHTML = "Game hasn't started";
-console.log("started script");
+let resetButton = document.getElementById("reset");
+resetButton.style.display = "none"
+
+let computerScore = 0;
+let playerScore = 0;
+document.getElementById("score").innerHTML = "You: " + playerScore + " Computer: " + computerScore;
+
+resetButton.addEventListener('click', resetGame);
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         playerSelection = button.id;
     });
     button.addEventListener('click', game);
-    console.log("addded event listener");
 });
+
+let buttonsArray = document.getElementsByClassName('buttons');
 
 function computerPlay() {
     let gameChoices = ["rock", "paper", "scissors"];
     let computerSelection = gameChoices[Math.floor(Math.random() * gameChoices.length)];
-    console.log("Computer selected");
     return computerSelection;
 }
 
+function checkScore() {
+    if (playerScore == 5 || computerScore == 5) {
+        resetButton.style.display = "block";
+
+        for (var i=0; i<buttonsArray.length; i++) {
+            buttonsArray[i].disabled = true;
+        }
+        
+        document.getElementById("pAgain").innerHTML = "Click the button below to play again!";    
+
+        if (playerScore == 5) {
+            document.getElementById('result').innerHTML = "Game over! You beat the computer!";  
+            
+        }
+        
+        if (computerScore == 5) {
+            document.getElementById("result").innerHTML = "Game over! The Computer beat you!"; 
+            
+        }
+    }    
+
+    else {
+        return;
+    }
+}
+
+function resetGame() {
+    resetButton.style.display = "none"; 
+    document.getElementById("pAgain").innerHTML = ""; 
+    document.getElementById("result").innerHTML = "";
+    computerScore = 0;
+    playerScore = 0;
+    document.getElementById("score").innerHTML = "You: " + playerScore + " Computer: " + computerScore;
+    for (var i=0; i<buttonsArray.length; i++) {
+        buttonsArray[i].disabled = false;
+    }     
+    return;
+}
+    
+
 function game() {
+    checkScore();
+    resetButton.style.display === "none"
     let computer = computerPlay(); 
     let userChoice = playerSelection;
-    console.log("assigned computer and user choice");
 
     if (userChoice === computer) {
-        document.getElementById("result").innerHTML = "You selected " + userChoice + ". The computer selected " + computer + ". It's a tie!";
-        console.log("It's a tie");
+        document.getElementById("result").innerHTML = "You selected " + userChoice + ". The computer selected " + computer + ".";
+        checkScore();
     }
 
     else if (userChoice === "rock") {
         if (computer === "paper") {
-            document.getElementById("result").innerHTML = "You selected " + userChoice + ". The computer selected " + computer + ". Computer wins!";
-        console.log("User chose rock, computer paper");
+            document.getElementById("result").innerHTML = "You selected " + userChoice + ". The computer selected " + computer + ".";
+            computerScore = computerScore + 1;
+            document.getElementById("score").innerHTML = "You: " + playerScore + " Computer: " + computerScore;
+            checkScore();
         }
 
         else if (computer === "scissors") {
-            document.getElementById("result").innerHTML = "You selected " + userChoice + ". The computer selected " + computer + ". You win!";
+            document.getElementById("result").innerHTML = "You selected " + userChoice + ". The computer selected " + computer + ".";
+            playerScore = playerScore + 1;
+            document.getElementById("score").innerHTML = "You: " + playerScore + " Computer: " + computerScore;
+            checkScore();
         }
     }
     else if (userChoice === "paper") {
         if (computer === "scissors") {
-            document.getElementById("result").innerHTML = "You selected " + userChoice + ". The computer selected " + computer + ". Computer wins!";
+            document.getElementById("result").innerHTML = "You selected " + userChoice + ". The computer selected " + computer + ".";
+            computerScore = computerScore + 1;
+            document.getElementById("score").innerHTML = "You: " + playerScore + " Computer: " + computerScore;
+            checkScore();
         }
 
         else if (computer === "rock") {
-            document.getElementById("result").innerHTML = "You selected " + userChoice + ". The computer selected " + computer + ". You win!";
+            document.getElementById("result").innerHTML = "You selected " + userChoice + ". The computer selected " + computer + ".";
+            playerScore = playerScore + 1;
+            document.getElementById("score").innerHTML = "You: " + playerScore + " Computer: " + computerScore;
+            checkScore();
         }
     }
 
     else if (userChoice === "scissors") {
         if (computer === "rock") {
             document.getElementById("result").innerHTML = "You selected " + userChoice + ". The computer selected " + computer + ". Computer wins!";
+            computerScore = computerScore + 1;
+            document.getElementById("score").innerHTML = "You: " + playerScore + " Computer: " + computerScore;
+            checkScore();
         }
 
         else if (computer === "paper") {
             document.getElementById("result").innerHTML = "You selected " + userChoice + ". The computer selected " + computer + ". You win!";
+            playerScore = playerScore + 1;
+            document.getElementById("score").innerHTML = "You: " + playerScore + " Computer: " + computerScore;
+            checkScore();
         }
     }
 }
+
 
